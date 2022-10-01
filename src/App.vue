9990af4ab443d04180f9fb95d85adaeaa2e16fcf
DellSilva <adelmo.s.jr@hotmail.com>
2022-10-01 00:05:29 -0300

    adicinado o componet LifeCycle

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
 <template>
-  <MainComponent/> 
+  <div>
+    <MainComponent/>
+    <LifeCycle/>    
+  </div> 
 </template>
 
 <script>
   import MainComponent from './components/MainComponent.vue';
+  import LifeCycle from './components/LifeCycle.vue';
 
   export default {
     name: "App",
     components: [
         MainComponent
     ],
-    components: { MainComponent }
+    components: { MainComponent, LifeCycle }
 }
 </script>
 
---
+++ src/components/LifeCycle.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>Meu nome: </h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'LifeCycle',
+        data() {
+            return {
+                nome: 'Ainda não sei'
+            }
+        }
+    }
+</script>
