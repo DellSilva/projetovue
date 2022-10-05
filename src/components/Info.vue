783758f1c8ba36b210c7a7057bd3a312aeb6f0f7
DellSilva <adelmo.s.jr@hotmail.com>
2022-10-04 22:47:10 -0300

    metodos e atributos

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -8,19 +8,38 @@
             <li>PHP</li>
             <li>Laravel</li>
         </ul>
+        <div>
+            <button @click="showEmail">{{ textoBotao }}</button>
+        </div>
         <p v-show="mostrar_email">mande mensagem para: {{email}}</p>
+        <p>click <a v-bind:href="link" target="_blank">aqui</a> para acessar o portifólio</p>
+        <Picture/>
     </div>
 </template>
 
 <script>
+import Picture from './Picture.vue';
     export default {
-        name: 'Info',
-        data(){
-            return {
-                esta_trabalhando: false,
-                mostrar_email: true,
-                email: 'adelmo.dev@gmail.com'
+    name: "Info",
+    data() {
+        return {
+            esta_trabalhando: false,
+            mostrar_email: false,
+            email: "adelmo.dev@gmail.com",
+            link: "https://www.google.com.br/",
+            textoBotao: 'Mostrar e-mail'
+        };
+    },
+    methods: {
+        showEmail() {
+            this.mostrar_email = !this.mostrar_email
+            if(!this.mostrar_email) {
+                this.textoBotao = 'Mostrar e-mail'
+            } else {
+                this.textoBotao = 'Ocultar e-mail'
             }
         }
-    }
+    },
+    components: { Picture }
+}
 </script>
