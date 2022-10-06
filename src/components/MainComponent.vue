7d3b97b3fbf3f32d0f59223927d29806fd2caa26
DellSilva <adelmo.s.jr@hotmail.com>
2022-10-06 20:42:11 -0300

    multiplos eventos

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -3,18 +3,21 @@
         <h1>Olá vue!</h1>
         <h2>agora sim !!!!!</h2>
         <p>Meu nome é {{ nome }} e sou um {{ profissao }} </p>
+        <MultiplosEventos />
     </div>
 
 </template>
 
 <script>
-    export default {
-        name: 'MainComponent',
-        data() {
-            return {
-                nome: "Dell Silva",
-                profissao: "dev junior"
-            }
-        }
-    }
+import MultiplosEventos from './MultiplosEventos.vue';
+export default {
+    name: "MainComponent",
+    data() {
+        return {
+            nome: "Dell Silva",
+            profissao: "dev junior"
+        };
+    },
+    components: { MultiplosEventos }
+}
 </script>
