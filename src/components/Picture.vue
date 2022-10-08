97d606a27a74da66f1f2bcf11ba0fe9b6b5f6e4d
DellSilva <adelmo.s.jr@hotmail.com>
2022-10-08 00:18:34 -0300

    emits

--- src/components/Picture.vue
+++ src/components/Picture.vue
@@ -1,15 +1,25 @@
 <template>
     <img :src="avatar" :alt="descricao">
+    <MudarImagem @mudar-imagem="trocarImagem"/>
 </template>
 
 <script>
+import MudarImagem from './MudarImagem.vue';
     export default {
-        name: 'Picture',
-        data() {
-            return {
-                avatar: "/img/avatar.png",
-                descricao: "Dell Silva"
-            }
+    name: "Picture",
+    data() {
+        return {
+            avatar: "/img/avatar.png",
+            descricao: "Dell Silva"
+        };
+    },
+    components: { 
+        MudarImagem 
+    },
+    methods: {
+        trocarImagem() {
+            this.avatar = "/img/avatar2.png"
         }
     }
+}
 </script>
